0475fd43c19f30d236971ba66b3f021b888f17b2
K1r1to75 <alexchita2008@mail.ru>
2026-05-24 15:05:42 +0900

    Add files via upload

---
+++ Sharpik/2026.05.14.massivi.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dzshka
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1");
+            int[] array1 = { 0, 5, 0, 3, 0, 8, 0 };
+            Console.WriteLine("Исходный массив: " + string.Join(", ", array1));
+            int index1 = 0;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != 0)
+                {
+                    array1[index1] = array1[i];
+                    index1++;
+                }
+            }
+
+            for (int i = index1; i < array1.Length; i++)
+            {
+                array1[i] = -1;
+            }
+
+            Console.WriteLine(string.Join(", ", array1));
+
+            Console.WriteLine("\nЗадание 2");
+            int[] array2 = { 3, -2, 5, -8, 0, -1, 4 };
+            Console.WriteLine("Исходный массив: " + string.Join(", ", array2));
+            int[] result2 = new int[array2.Length];
+            int index2 = 0;
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                if (array2[i] < 0)
+                {
+                    result2[index2] = array2[i];
+                    index2++;
+                }
+            }
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                if (array2[i] >= 0)
+                {
+                    result2[index2] = array2[i];
+                    index2++;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", result2));
+
+            Console.WriteLine("\nЗадание 3");
+            int[] array3 = { 1, 5, 3, 5, 7, 5, 9 };
+            Console.WriteLine("Исходный массив: " + string.Join(", ", array3));
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 0; i < array3.Length; i++)
+            {
+                if (array3[i] == number)
+                    count++;
+            }
+
+            Console.WriteLine($"Число {number} встречается {count} раз(а)");
+
+            Console.WriteLine("\nЗадание 4");
+            int[,] array4 = {
+            { 1, 2, 3, 4 },
+            { 5, 6, 7, 8 },
+            { 9, 0, 2, 0 }
+        };
+
+            int rows = array4.GetLength(0);
+            int cols = array4.GetLength(1);
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                    Console.Write(array4[i, j] + " ");
+                Console.WriteLine();
+            }
+
+            int col1;
+            string input1;
+            while (true)
+            {
+                Console.Write("Номер первого столбца (от 0): ");
+                input1 = Console.ReadLine();
+                if (int.TryParse(input1, out col1) && col1 >= 0 && col1 < cols)
+                    break;
+                Console.WriteLine("Ошибка. Введите целое число от 0 до " + (cols - 1));
+            }
+
+
+            int col2;
+            string input2;
+            while (true)
+            {
+                Console.Write("Номер второго столбца (от 0): ");
+                input2 = Console.ReadLine();
+                if (int.TryParse(input2, out col2) && col2 >= 0 && col2 < cols)
+                    break;
+                Console.WriteLine("Ошибка. Введите целое число от 0 до " + (cols - 1));
+            }
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                int temp = array4[i, col1];
+                array4[i, col1] = array4[i, col2];
+                array4[i, col2] = temp;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                    Console.Write(array4[i, j] + " ");
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
---
+++ Sharpik/2026.05.15.modul'.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dzshka
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Задание 1");
+            int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 4, 6 };
+            int even = 0, odd = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] % 2 == 0) even++;
+                else odd++;
+            }
+            int uniqueCount = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                bool isUnique = true;
+                for (int j = 0; j < arr1.Length; j++)
+                {
+                    if (i != j && arr1[i] == arr1[j])
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique) uniqueCount++;
+            }
+            Console.WriteLine("Чётные: " + even + ", Нечётные: " + odd + ", Уникальные: " + uniqueCount);
+
+            Console.WriteLine("\nЗадание 2");
+            int[] arr2 = { 1, 5, 3, 9, 2, 8, 4, 12, 6 };
+            Console.Write("Введите число: ");
+            int num = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < arr2.Length; i++)
+                if (arr2[i] < num) count++;
+            Console.WriteLine("Меньше " + num + ": " + count);
+
+            Console.WriteLine("\nЗадание 3");
+            int[] arr3 = { 7, 6, 5, 3, 4, 7, 6, 5, 8, 7, 6, 5 };
+            Console.WriteLine("Массив: 7 6 5 3 4 7 6 5 8 7 6 5");
+            Console.Write("Введите три числа через пробел: ");
+            string[] input = Console.ReadLine().Split(' ');
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            int c = int.Parse(input[2]);
+            int seqCount = 0;
+            for (int i = 0; i < arr3.Length - 2; i++)
+                if (arr3[i] == a && arr3[i + 1] == b && arr3[i + 2] == c)
+                    seqCount++;
+            Console.WriteLine("Повторений: " + seqCount);
+
+            Console.WriteLine("\nЗадание 4");
+            int[] m = { 1, 2, 3, 4, 5 };
+            int[] n = { 3, 4, 5, 6, 7 };
+            int[] temp = new int[Math.Min(m.Length, n.Length)];
+            int tempIndex = 0;
+            for (int i = 0; i < m.Length; i++)
+            {
+                for (int j = 0; j < n.Length; j++)
+                {
+                    if (m[i] == n[j])
+                    {
+                        bool alreadyAdded = false;
+                        for (int k = 0; k < tempIndex; k++)
+                        {
+                            if (temp[k] == m[i])
+                            {
+                                alreadyAdded = true;
+                                break;
+                            }
+                        }
+                        if (!alreadyAdded)
+                        {
+                            temp[tempIndex] = m[i];
+                            tempIndex++;
+                        }
+                        break;
+                    }
+                }
+            }
+            Console.Write("Общие элементы: ");
+            for (int i = 0; i < tempIndex; i++)
+                Console.Write(temp[i] + " ");
+            Console.WriteLine();
+
+            Console.WriteLine("\nЗадание 5");
+            int[,] matrix = { { 3, 8, 1 }, { 12, 5, 9 }, { 2, 7, 4 } };
+            int min = matrix[0, 0], max = matrix[0, 0];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (matrix[i, j] < min) min = matrix[i, j];
+                    if (matrix[i, j] > max) max = matrix[i, j];
+                }
+            }
+            Console.WriteLine("Минимум: " + min + ", Максимум: " + max);
+
+            Console.WriteLine("\nЗадание 6");
+            Console.Write("Введите предложение: ");
+            string str6 = Console.ReadLine();
+            int words = 0;
+            bool inWord = false;
+            for (int i = 0; i < str6.Length; i++)
+            {
+                if (str6[i] != ' ')
+                {
+                    if (!inWord)
+                    {
+                        words++;
+                        inWord = true;
+                    }
+                }
+                else
+                {
+                    inWord = false;
+                }
+            }
+            Console.WriteLine("Слов: " + words);
+
+            Console.WriteLine("\nЗадание 7");
+            Console.Write("Введите предложение: ");
+            string str7 = Console.ReadLine();
+            string result = "";
+            string word = "";
+            for (int i = 0; i < str7.Length; i++)
+            {
+                if (str7[i] != ' ')
+                {
+                    word = str7[i] + word;
+                }
+                else
+                {
+                    result += word + " ";
+                    word = "";
+                }
+            }
+            result += word;
+            Console.WriteLine("Результат: " + result);
+
+            Console.WriteLine("\nЗадание 8");
+            Console.Write("Введите предложение: ");
+            string str8 = Console.ReadLine().ToLower();
+            int vowels = 0;
+            string vowelsList = "aeiouаеёиоуыэюя";
+            for (int i = 0; i < str8.Length; i++)
+            {
+                for (int j = 0; j < vowelsList.Length; j++)
+                {
+                    if (str8[i] == vowelsList[j])
+                    {
+                        vowels++;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Гласных: " + vowels);
+
+            Console.WriteLine("\nЗадание 9");
+            Console.Write("Введите строку: ");
+            string source = Console.ReadLine();
+            Console.Write("Введите подстроку: ");
+            string sub = Console.ReadLine();
+            int occ = 0;
+            for (int i = 0; i <= source.Length - sub.Length; i++)
+            {
+                bool match = true;
+                for (int j = 0; j < sub.Length; j++)
+                {
+                    if (source[i + j] != sub[j])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) occ++;
+            }
+            Console.WriteLine("Вхождений: " + occ);
+        }
+    }
+}
