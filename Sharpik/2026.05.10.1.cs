f4837fecbe684473476ea03297792ada12301708
K1r1to75 <alexchita2008@mail.ru>
2026-05-24 12:43:57 +0900

    Add files via upload

---
+++ Sharpik/2026.05.10.1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dzshka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1: Квадраты на прямоугольнике
+            Console.WriteLine("Задание 1");
+            T1();
+
+            Console.WriteLine("\nЗадание 2");
+            T2();
+
+            Console.WriteLine("\nЗадание 3");
+            T3();
+
+            Console.WriteLine("\nЗадание 4");
+            T4();
+
+            Console.WriteLine("\nЗадание 4");
+            T5();
+
+            Console.WriteLine("\nЗадание 4");
+            T6();
+
+            Console.WriteLine("\nЗадание 4");
+            T7();
+
+            Console.WriteLine("\nНажмите для выхода");
+            Console.ReadKey();
+        }
+
+        static void T1()
+        {
+            Console.Write("Введите число от 1 до 100: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 1 || number > 100)
+            {
+                Console.WriteLine("Ошибка: число должно быть в диапазоне от 1 до 100");
+            }
+            else if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        static void T2()
+        {
+            Console.Write("Введите число: ");
+            double value = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите процент: ");
+            double percent = double.Parse(Console.ReadLine());
+
+            double result = value * percent / 100;
+            Console.WriteLine($"Результат: {result}");
+        }
+
+        static void T3()
+        {
+            Console.Write("Введите первую цифру: ");
+            int d1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите вторую цифру: ");
+            int d2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите третью цифру: ");
+            int d3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите четвертую цифру: ");
+            int d4 = int.Parse(Console.ReadLine());
+
+            int formedNumber = d1 * 1000 + d2 * 100 + d3 * 10 + d4;
+            Console.WriteLine($"Сформированное число: {formedNumber}");
+        }
+
+        static void T4()
+        {
+            Console.Write("Введите шестизначное число: ");
+            string input = Console.ReadLine();
+
+            if (input.Length != 6 || !long.TryParse(input, out _))
+            {
+                Console.WriteLine("Ошибка: введено не шестизначное число");
+                return;
+            }
+
+            Console.Write("Введите первый номер разряда (1-6): ");
+            int pos1 = int.Parse(Console.ReadLine()) - 1;
+
+            Console.Write("Введите второй номер разряда (1-6): ");
+            int pos2 = int.Parse(Console.ReadLine()) - 1;
+
+            if (pos1 < 0 || pos1 > 5 || pos2 < 0 || pos2 > 5)
+            {
+                Console.WriteLine("Ошибка: номера разрядов должны быть от 1 до 6");
+                return;
+            }
+
+            char[] digits = input.ToCharArray();
+            char temp = digits[pos1];
+            digits[pos1] = digits[pos2];
+            digits[pos2] = temp;
+
+            Console.WriteLine($"Результат: {new string(digits)}");
+        }
+
+        static void T5()
+        {
+            Console.Write("Введите дату в формате д.м.г: ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+
+            string dayOfWeek = date.DayOfWeek.ToString();
+            string season;
+            int month = date.Month;
+
+            if (month == 12 || month == 1 || month == 2)
+                season = "Winter";
+            else if (month >= 3 && month <= 5)
+                season = "Spring";
+            else if (month >= 6 && month <= 8)
+                season = "Summer";
+            else
+                season = "Autumn";
+
+            Console.WriteLine($"{season} {dayOfWeek}");
+        }
+
+        static void T6()
+        {
+            Console.Write("Выберите направление:\n1 - Цельсий в Фаренгейт\n2 - Фаренгейт в Цельсий: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите температуру: ");
+            double temp = double.Parse(Console.ReadLine());
+
+            if (choice == 1)
+                Console.WriteLine($"{temp}°C = {temp * 9 / 5 + 32}°F");
+            else if (choice == 2)
+                Console.WriteLine($"{temp}°F = {(temp - 32) * 5 / 9}°C");
+            else
+                Console.WriteLine("Неверный выбор");
+        }
+
+        static void T7()
+        {
+            Console.Write("Введите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int start = Math.Min(a, b);
+            int end = Math.Max(a, b);
+
+            Console.WriteLine($"Четные числа от {start} до {end}:");
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                    Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Sharpik/2026.05.13.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dzshka
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Задание 1
+            DrawSquare(5, '*');
+
+            // Задание 2
+            Console.WriteLine(IsPalindrome(1221));
+            Console.WriteLine(IsPalindrome(3443));
+            Console.WriteLine(IsPalindrome(7854));
+
+            // Задание 3
+            int[] original = { 1, 2, 6, -1, 88, 7, 6 };
+            int[] filter = { 6, 88, 7 };
+            int[] result = FilterArray(original, filter);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        static void DrawSquare(int length, char symbol)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(new string(symbol, length));
+            }
+        }
+
+        static bool IsPalindrome(int number)
+        {
+            string s = number.ToString();
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                    return false;
+            }
+            return true;
+        }
+
+        static int[] FilterArray(int[] original, int[] filter)
+        {
+            int count = 0;
+            for (int i = 0; i < original.Length; i++)
+            {
+                bool remove = false;
+                for (int j = 0; j < filter.Length; j++)
+                {
+                    if (original[i] == filter[j])
+                    {
+                        remove = true;
+                        break;
+                    }
+                }
+                if (!remove) count++;
+            }
+
+            int[] result = new int[count];
+            int index = 0;
+            for (int i = 0; i < original.Length; i++)
+            {
+                bool remove = false;
+                for (int j = 0; j < filter.Length; j++)
+                {
+                    if (original[i] == filter[j])
+                    {
+                        remove = true;
+                        break;
+                    }
+                }
+                if (!remove)
+                {
+                    result[index] = original[i];
+                    index++;
+                }
+            }
+            return result;
+        }
+    }
+}
