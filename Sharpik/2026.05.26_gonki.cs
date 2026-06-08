45e221886e6ec895f1bc3411121d9657535b53e3
K1r1to75 <alexchita2008@mail.ru>
2026-06-08 21:55:28 +0900

    Add files via upload

---
+++ Sharpik/2026.05.26_gonki.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Dzshka
+{
+    public delegate void StatusUpdateHandler(string statusInfo);
+    public delegate void VictoryHandler(string championName);
+
+    public abstract class Racer
+    {
+        public string Model { get; set; }
+        public int Distance { get; set; }
+        public int Velocity { get; set; }
+
+        public event StatusUpdateHandler OnStatusUpdate;
+        public event VictoryHandler OnVictory;
+
+        public Racer(string model)
+        {
+            Model = model;
+            Distance = 0;
+            Random generator = new Random();
+            Velocity = generator.Next(10, 30);
+        }
+
+        public abstract void Advance();
+
+        protected void BroadcastStatus(string info)
+        {
+            OnStatusUpdate?.Invoke(info);
+        }
+
+        protected void DeclareVictory()
+        {
+            OnVictory?.Invoke(Model);
+        }
+    }
+
+    public class RacingCar : Racer
+    {
+        public RacingCar() : base("Гоночный болид") { }
+
+        public override void Advance()
+        {
+            Random generator = new Random();
+            Velocity += generator.Next(-5, 10);
+
+            NormalizeSpeed(1, 50);
+
+            Distance += Velocity;
+            BroadcastStatus($"[Болид] {Model} преодолел {Distance} км. Текущая скорость: {Velocity} км/ч");
+
+            if (Distance >= 100) DeclareVictory();
+        }
+
+        private void NormalizeSpeed(int min, int max)
+        {
+            if (Velocity < min) Velocity = min;
+            if (Velocity > max) Velocity = max;
+        }
+    }
+
+    public class Sedan : Racer
+    {
+        public Sedan() : base("Седан") { }
+
+        public override void Advance()
+        {
+            Random generator = new Random();
+            Velocity += generator.Next(-3, 5);
+            if (Velocity < 1) Velocity = 1;
+
+            Distance += Velocity;
+            BroadcastStatus($"[Седан] {Model} проехал {Distance} км. Скорость: {Velocity} км/ч");
+
+            if (Distance >= 100) DeclareVictory();
+        }
+    }
+
+    public class Lorry : Racer
+    {
+        public Lorry() : base("Фура") { }
+
+        public override void Advance()
+        {
+            Random generator = new Random();
+            Velocity += generator.Next(-2, 3);
+            if (Velocity < 1) Velocity = 1;
+
+            Distance += Velocity;
+
+            Distance = Math.Min(Distance, 100);
+
+            BroadcastStatus($"[Фура] {Model} проехала {Distance} км. Скорость: {Velocity} км/ч");
+
+            if (Distance >= 100) DeclareVictory();
+        }
+    }
+
+    public class Minibus : Racer
+    {
+        public Minibus() : base("Микроавтобус") { }
+
+        public override void Advance()
+        {
+            Random generator = new Random();
+            int acceleration = generator.Next(-4, 6);
+            Velocity += acceleration;
+
+            Distance += Velocity;
+            BroadcastStatus($"[Микроавтобус] {Model} проехал {Distance} км. Скорость: {Velocity} км/ч");
+
+            if (Distance >= 100) DeclareVictory();
+        }
+    }
+
+    public class Tournament
+    {
+        private List<Racer> participants = new List<Racer>();
+        private bool competitionFinished = false;
+        private int roundCount = 0; // Счетчик раундов
+
+        public void RegisterRacer(Racer racer)
+        {
+            racer.OnStatusUpdate += DisplayStatus;
+            racer.OnVictory += AnnounceChampion;
+            participants.Add(racer);
+        }
+
+        private void DisplayStatus(string message)
+        {
+            if (!competitionFinished)
+                Console.WriteLine($"Раунд {roundCount}: {message}");
+        }
+
+        private void AnnounceChampion(string championModel)
+        {
+            if (!competitionFinished)
+            {
+                competitionFinished = true;
+                Console.WriteLine("  Финиш");
+                Console.WriteLine($"  Победитель: {championModel}");
+                Console.WriteLine($"  Всего раундов: {roundCount}");
+            }
+        }
+
+        public void BeginTournament()
+        {
+            Console.WriteLine("Гран-при");
+            Console.WriteLine("Нажмите Enter для старта");
+            Console.ReadLine();
+            Console.Clear();
+
+            while (!competitionFinished)
+            {
+                roundCount++;
+                Console.WriteLine($"\nРаунд {roundCount}");
+
+                foreach (var racer in participants)
+                {
+                    if (!competitionFinished)
+                    {
+                        racer.Advance();
+                    }
+                }
+
+                Thread.Sleep(700);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tournament tournament = new Tournament();
+
+            tournament.RegisterRacer(new RacingCar());
+            tournament.RegisterRacer(new Sedan());
+            tournament.RegisterRacer(new Lorry());
+            tournament.RegisterRacer(new Minibus());
+
+            tournament.BeginTournament();
+
+            Console.WriteLine("\nГонка завершена!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sharpik/2026.06.01.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Dzshka
+{
+    using System;
+    using System.Collections.Generic;
+
+    namespace FinancialUtilities
+    {
+        public class NegativeValueException : Exception
+        {
+            public NegativeValueException(string message) : base(message) { }
+            public NegativeValueException(string message, Exception inner) : base(message, inner) { }
+        }
+
+        public class OverdraftException : Exception
+        {
+            public OverdraftException(string message) : base(message) { }
+            public OverdraftException(string message, Exception inner) : base(message, inner) { }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Калькулятор деления \n");
+                ExecuteSafeCalculator();
+
+
+                Console.WriteLine("Нажмите любую клавишу для продолжения");
+                Console.ReadKey();
+                Console.Clear();
+
+                Console.WriteLine("\n Работа приложения завершена ");
+                Console.ReadKey();
+            }
+
+            static void ExecuteSafeCalculator()
+            {
+                List<int> history = new List<int>();
+                bool keepRunning = true;
+
+                while (keepRunning)
+                {
+                    try
+                    {
+                        Console.Write("Введите первое число: ");
+                        int dividend = int.Parse(Console.ReadLine());
+
+                        Console.Write("Введите второе число: ");
+                        int divisor = int.Parse(Console.ReadLine());
+
+                        int quotient = dividend / divisor;
+
+                        history.Add(quotient);
+
+                        Console.WriteLine($"Результат: {quotient}");
+
+                        Console.Write("Хотмте продолжить? (да/нет): ");
+                        string response = Console.ReadLine().Trim().ToLower();
+                        if (response != "да")
+                            keepRunning = false;
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Ошибка: попытка деления на ноль");
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Ошибка: требуется числовое значение");
+                    }
+                    finally
+                    {
+                        Console.WriteLine("Попытка выполнения операции завершена");
+                    }
+                }
+
+                Console.WriteLine("\nЖурнал успешных операций:");
+                for (int i = 0; i < history.Count; i++)
+                {
+                    Console.WriteLine($"Операция {i + 1}: {history[i]}");
+                }
+            }
+        }
+    }
+}
